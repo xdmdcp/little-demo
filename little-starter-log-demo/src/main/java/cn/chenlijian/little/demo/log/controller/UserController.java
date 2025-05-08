91a2a7c60b29dac9398210a82a0ce9ecec292c11
chenlijian <yujian95.cn@163.com>
2025-05-08 16:22:28 +0800

    feat(demo): 添加 little-starter-log-demo 模块
    - 新增 little-starter-log-demo 模块用于演示日志功能
    - 更新 pom.xml 文件，添加新模块和相关依赖
    - 创建应用入口类、配置文件和用户控制器- 集成 Spring Boot 和 little-starter-log

---
+++ little-starter-log-demo/src/main/java/cn/chenlijian/little/demo/log/controller/UserController.java
@@ -0,0 +1,19 @@
+package cn.chenlijian.little.demo.log.controller;
+
+import cn.chenlijian.little.common.annotation.log.ApiLog;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class UserController {
+
+    @ApiLog(value = "hello")
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", required = false) String name) {
+        log.info("hello " + name);
+        return "hello " + name;
+    }
+}
