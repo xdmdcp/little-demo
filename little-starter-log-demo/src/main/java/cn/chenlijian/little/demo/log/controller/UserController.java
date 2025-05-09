67cfab6d421bf79eb085239f42af5ee2e3f1b1df
chenlijian <yujian95.cn@163.com>
2025-05-09 10:55:08 +0800

    test(little-starter-log-demo): 重命名配置文件并调整日志设置
    - 将 application-dev.yml 重命名为 application-test.yml
    - 更新 application.yml 中的 profiles 设置
    - 修改 application-test.yml 中的日志配置
    - 在 UserController 中添加新的测试用例接口

--- little-starter-log-demo/src/main/java/cn/chenlijian/little/demo/log/controller/UserController.java
+++ little-starter-log-demo/src/main/java/cn/chenlijian/little/demo/log/controller/UserController.java
@@ -3,16 +3,42 @@ package cn.chenlijian.little.demo.log.controller;
 import cn.chenlijian.little.common.annotation.log.ApiLog;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
+@RequestMapping("/user")
 public class UserController {
 
     @ApiLog(value = "hello")
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", required = false) String name) {
         return "hello " + name;
+    }
+
+    @ApiLog(value = "skip")
+    @GetMapping("/skip")
+    public String skip(@RequestParam(value = "name", required = false) String name) {
+        return "skip " + name;
+    }
+
+    @ApiLog(value = "skip1")
+    @GetMapping("/skip1")
+    public String skip1(@RequestParam(value = "name", required = false) String name) {
+        return "skip1 " + name;
+    }
+
+    @ApiLog(value = "skip2")
+    @GetMapping("/skip/2")
+    public String skip2(@RequestParam(value = "name", required = false) String name) {
+        return "skip2 " + name;
+    }
+
+    @ApiLog(value = "skip3")
+    @GetMapping("/skip/3")
+    public String skip3(@RequestParam(value = "name", required = false) String name) {
+        return "skip3 " + name;
     }
 }
